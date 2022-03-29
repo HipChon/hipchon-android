493ac1a016c97f395a9d0011ed9d6c4500d48e97
dddooo9 <dlaehdus99@naver.com>
2022-03-30 03:34:32 +0900

    [FEATURE] 다크모드 비활성화

--- app/src/main/java/com/gritbus/hipchon/HipChonApplication.kt
+++ app/src/main/java/com/gritbus/hipchon/HipChonApplication.kt
@@ -1,6 +1,7 @@
 package com.gritbus.hipchon
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
@@ -10,6 +11,9 @@ class HipChonApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(BuildConfig.MAP_CLIENT_ID)
         KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
