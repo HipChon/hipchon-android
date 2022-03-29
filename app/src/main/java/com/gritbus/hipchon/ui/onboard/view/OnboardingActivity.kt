27c2f90eccf4f28a195fddf2261c47a0a54362ee
dddooo9 <dlaehdus99@naver.com>
2022-03-30 00:59:52 +0900

    [FEATURE] 네이버 login 구현

--- app/src/main/java/com/gritbus/hipchon/ui/onboard/view/OnboardingActivity.kt
+++ app/src/main/java/com/gritbus/hipchon/ui/onboard/view/OnboardingActivity.kt
@@ -1,9 +1,17 @@
 package com.gritbus.hipchon.ui.onboard.view
 
 import android.os.Bundle
+import android.util.Log
+import com.gritbus.hipchon.BuildConfig
 import com.gritbus.hipchon.R
 import com.gritbus.hipchon.databinding.ActivityOnboardingBinding
 import com.gritbus.hipchon.utils.BaseViewUtil
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.log.NidLog
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 class OnboardingActivity :
     BaseViewUtil.BaseAppCompatActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
@@ -14,5 +22,57 @@ class OnboardingActivity :
     }
 
     override fun initView() {
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        binding.mbOnboardingNaver.setOnClickListener {
+            setNaverLogin()
+        }
+    }
+
+    private fun setNaverLogin() {
+        NidLog.init()
+        NaverIdLoginSDK.initialize(
+            this,
+            BuildConfig.NAVER_CLIENT_ID,
+            BuildConfig.NAVER_CLIENT_SECRET,
+            BuildConfig.NAVER_APP_NAME
+        )
+
+        val oauthLoginCallback = object : OAuthLoginCallback {
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDesc = NaverIdLoginSDK.getLastErrorDescription()
+                Log.e("naver login error : ", "errorCode:$errorCode, errorDesc:$errorDesc")
+            }
+
+            override fun onSuccess() {
+                getProfile()
+            }
+        }
+        NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
+    }
+
+    private fun getProfile() {
+        NidOAuthLogin().callProfileApi(object: NidProfileCallback<NidProfileResponse> {
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDesc = NaverIdLoginSDK.getLastErrorDescription()
+                Log.e("naver login error : ", "errorCode:$errorCode, errorDesc:$errorDesc")
+            }
+
+            override fun onSuccess(result: NidProfileResponse) {
+                result.profile?.id?.let { Log.i("unique id", it) }
+            }
+        })
     }
 }
