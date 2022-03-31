4e3adaad437b19f592cdf866d0ea0d82a5059b22
dddooo9 <dlaehdus99@naver.com>
2022-04-01 01:10:46 +0900

    [FIX] 카카오 로그인/로그아웃 토큰 처리

--- app/src/main/java/com/gritbus/hipchon/ui/my/view/MySettingActivity.kt
+++ app/src/main/java/com/gritbus/hipchon/ui/my/view/MySettingActivity.kt
@@ -3,12 +3,14 @@ package com.gritbus.hipchon.ui.my.view
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.gritbus.hipchon.R
 import com.gritbus.hipchon.databinding.ActivityMySettingBinding
 import com.gritbus.hipchon.ui.my.viewmodel.MySettingViewModel
 import com.gritbus.hipchon.ui.onboard.view.OnboardingActivity
 import com.gritbus.hipchon.utils.BaseViewUtil
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,18 +36,34 @@ class MySettingActivity :
     private fun setObserver() {
         viewModel.isLogoutSuccess.observe(this) {
             if (it) {
-                startActivity(Intent(baseContext, OnboardingActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                })
+                UserApiClient.instance.logout { error ->
+                    if (error != null) {
+                        Log.e(this.javaClass.name, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                    }
+                    else {
+                        Log.i(this.javaClass.name, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                        startActivity(Intent(baseContext, OnboardingActivity::class.java).apply {
+                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        })
+                    }
+                }
             }
         }
         viewModel.isLeaveSuccess.observe(this) {
             if (it) {
-                startActivity(Intent(baseContext, OnboardingActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                })
+                UserApiClient.instance.unlink { error ->
+                    if (error != null) {
+                        Log.e(this.javaClass.name, "연결 끊기 실패", error)
+                    }
+                    else {
+                        Log.i(this.javaClass.name, "연결 끊기 성공. SDK에서 토큰 삭제 됨")
+                        startActivity(Intent(baseContext, OnboardingActivity::class.java).apply {
+                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        })
+                    }
+                }
             }
         }
     }
