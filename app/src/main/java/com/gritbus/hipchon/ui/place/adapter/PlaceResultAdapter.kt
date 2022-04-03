0ff22607d19541cdf4f276db5ebc5bc2ffc6cfbe
dddooo9 <dlaehdus99@naver.com>
2022-04-03 23:59:32 +0900

    [FEATURE] 공간 검색 결과의 지역 정보, 키워드 정보 연결

--- app/src/main/java/com/gritbus/hipchon/ui/place/adapter/PlaceResultAdapter.kt
+++ app/src/main/java/com/gritbus/hipchon/ui/place/adapter/PlaceResultAdapter.kt
@@ -1,5 +1,10 @@
 package com.gritbus.hipchon.ui.place.adapter
 
+import android.content.res.ColorStateList
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
@@ -9,8 +14,13 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.gritbus.hipchon.R
+import com.gritbus.hipchon.data.model.place.KeywordItem
 import com.gritbus.hipchon.data.model.place.PlaceSearchAllDataItem
 import com.gritbus.hipchon.databinding.ItemPlaceResultBinding
+import com.gritbus.hipchon.domain.model.Keyword
+import com.gritbus.hipchon.domain.model.KeywordFacility
+import com.gritbus.hipchon.domain.model.KeywordMood
+import com.gritbus.hipchon.domain.model.KeywordSatisfaction
 
 class PlaceResultAdapter(
     private val fragmentManager: FragmentManager,
@@ -52,25 +62,79 @@ class PlaceResultAdapter(
             }
             binding.vpHomePlaceThumbnail.adapter =
                 PlaceResultImageAdapter(placeData.imageList, fragmentManager, lifecycle)
-            binding.ivHomePlaceSaveCount.background = when (placeData.isMyplace) {
-                true -> ContextCompat.getDrawable(binding.root.context, R.drawable.ic_save_filled)
-                false -> ContextCompat.getDrawable(binding.root.context, R.drawable.ic_save)
-            }
-            binding.tvHomePlaceTitle.text = placeData.name
+
+            setTitleSpannable(placeData)
+
             binding.tvHomePlaceSaveCount.text = placeData.myplaceCnt.toString()
             binding.tvHomePlaceFeedCount.text = placeData.postCnt.toString()
+            placeData.keyword?.let { setKeyword(it) }
 
             binding.executePendingBindings()
+        }
+
+        private fun setKeyword(keyword: KeywordItem) {
+            binding.tvHomePlaceKeyword.text = keyword.keyword
+            val backgroundTint = when (keyword.category) {
+                Keyword.FACILITY.value -> {
+                    R.color.secondary_yellow
+                }
+                Keyword.SATISFACTION.value -> {
+                    R.color.primary_green
+                }
+                else -> {
+                    R.color.secondary_blue
+                }
+            }
+            when (keyword.keywordId) {
+                in (1..5) -> {
+                    binding.ivHomePlaceKeyword.setImageResource(KeywordFacility.values()[keyword.keywordId - 1].iconDrawable)
+                }
+                in (6..10) -> {
+                    binding.ivHomePlaceKeyword.setImageResource(KeywordMood.values()[keyword.keywordId - 5 - 1].iconDrawable)
+                }
+                else -> {
+                    binding.ivHomePlaceKeyword.setImageResource(KeywordSatisfaction.values()[keyword.keywordId - 10 - 1].iconDrawable)
+                }
+            }
+            binding.llHomePlaceKeyword.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(binding.root.context, backgroundTint))
+        }
+
+        private fun setTitleSpannable(placeData: PlaceSearchAllDataItem) {
+            val title = placeData.name + "   " + placeData.city
+            val spannableBuilder = SpannableStringBuilder(title)
+            val colorGray =
+                ForegroundColorSpan(ContextCompat.getColor(binding.root.context, R.color.gray04))
+            val size16Span = RelativeSizeSpan(8f / 9f)
+            spannableBuilder.setSpan(
+                colorGray,
+                placeData.name.length,
+                title.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            spannableBuilder.setSpan(
+                size16Span,
+                placeData.name.length,
+                title.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            binding.tvHomePlaceTitle.text = spannableBuilder
         }
     }
 
     companion object {
         val diffUtil = object : DiffUtil.ItemCallback<PlaceSearchAllDataItem>() {
-            override fun areItemsTheSame(oldItem: PlaceSearchAllDataItem, newItem: PlaceSearchAllDataItem): Boolean {
+            override fun areItemsTheSame(
+                oldItem: PlaceSearchAllDataItem,
+                newItem: PlaceSearchAllDataItem
+            ): Boolean {
                 return oldItem.placeId == newItem.placeId
             }
 
-            override fun areContentsTheSame(oldItem: PlaceSearchAllDataItem, newItem: PlaceSearchAllDataItem): Boolean {
+            override fun areContentsTheSame(
+                oldItem: PlaceSearchAllDataItem,
+                newItem: PlaceSearchAllDataItem
+            ): Boolean {
                 return oldItem == newItem
             }
         }
