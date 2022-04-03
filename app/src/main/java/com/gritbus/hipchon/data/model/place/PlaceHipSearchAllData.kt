359a79565fa65388f94e940b29f8618a9437476a
dddooo9 <dlaehdus99@naver.com>
2022-04-03 16:02:07 +0900

    [FIX] place Response 변경된 이름 수정

--- app/src/main/java/com/gritbus/hipchon/data/model/place/PlaceHipSearchAllData.kt
+++ app/src/main/java/com/gritbus/hipchon/data/model/place/PlaceHipSearchAllData.kt
@@ -5,11 +5,11 @@ class PlaceHipSearchAllData : ArrayList<PlaceHipSearchAllDataItem>()
 data class PlaceHipSearchAllDataItem(
     val category: String,
     val city: String,
+    val image: String,
     val isMyplace: Boolean,
     val keyword: KeywordItem,
     val myplaceCnt: Int,
     val name: String,
     val placeId: Int,
-    val placeImage: String,
     val postCnt: Int
 )
